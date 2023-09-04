74aebfe7bdf3a61acc5e15a04dfb4298dd392d8c
노성호 <shtjdgh00@gmail.com>
2023-09-04 11:21:31 +0900

    Feat: 프로잭트 생성 프로젝트 생성 및 git ignore 작성 Resolves: S09P22A304-28

---
+++ backend/src/main/java/com/ssafy/yesrae/YesraeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.yesrae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesraeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesraeApplication.class, args);
+	}
+
+}
