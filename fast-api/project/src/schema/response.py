1d88c34b7974fd91a7410e6f5cf4eb48b667583d
김도현 <beigemint26@gmail.com>
2023-09-22 17:40:53 +0900

    Feat : 노맨틀 API 추가 진행

--- fast-api/project/src/schema/response.py
+++ fast-api/project/src/schema/response.py
@@ -1,16 +1,37 @@
 
 from pydantic import BaseModel
 
-
+"""
+TODO album_img 추가
+노래 제목 추측 결과
+"""
 class SongQuizSchema(BaseModel):
     id : str
     name : str
     similarity : float
     rank : str
+    album_img : str
 
     class Config:
         from_attributes = True
 
 
 
+"""
+노래 제목 입력 결과
+"""
+class SongTitleSchema(BaseModel):
+    name : str
 
+
+"""
+전체 정답곡과 유사도 높은 1000곡 결과
+"""
+class SongTotalRankSchema(BaseModel):
+    singer : str
+    rank : str
+    similarity : float
+
+    class Config:
+        from_attributes = True
+    
