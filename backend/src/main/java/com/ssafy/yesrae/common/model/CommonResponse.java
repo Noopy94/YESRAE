f4ada5b53fa6d37529af9ca21f45c806395f474f
노성호 <shtjdgh00@gmail.com>
2023-09-25 11:14:17 +0900

    Feat: common response 멤버 이름 변경
    common response 멤버 변수인 data를 content로 변수명 변경

--- backend/src/main/java/com/ssafy/yesrae/common/model/CommonResponse.java
+++ backend/src/main/java/com/ssafy/yesrae/common/model/CommonResponse.java
@@ -24,13 +24,13 @@ public final class CommonResponse<T> {
     // 성공 여부를 나타내는 boolean 변수
     private final boolean success;
     // 클라이언트가 요청한 데이터. 요청 실패했을 경우 null
-    private final T data;
+    private final T content;
     // 실패했을 경우 어떤 에러인지 클라이언트에게 전달. 요청 성공했을 경우 null
     private final Error error;
 
-    public CommonResponse(boolean success, T data, Error error) {
+    public CommonResponse(boolean success, T content, Error error) {
         this.success = success;
-        this.data = data;
+        this.content = content;
         this.error = error;
     }
 
